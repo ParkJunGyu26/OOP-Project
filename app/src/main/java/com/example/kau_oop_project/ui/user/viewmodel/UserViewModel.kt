02e798944ada461e5ba0ecec41143502288ef07c
박준규 <junju404@naver.com>
2024-12-17 09:08:27 +0900

    fix: currentUser의 타입이 User에서 String 일반 형태로 변경. uid만 사용할 수 있도록

--- app/src/main/java/com/example/kau_oop_project/ui/user/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/kau_oop_project/ui/user/viewmodel/UserViewModel.kt
@@ -3,27 +3,45 @@ package com.example.kau_oop_project.ui.user.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.kau_oop_project.data.model.response.LoginResponse
-import com.example.kau_oop_project.data.model.User
+import com.example.kau_oop_project.data.model.response.UserInfo
+import com.example.kau_oop_project.data.model.response.UserResponse
+import com.example.kau_oop_project.repository.UserRepository
+import kotlinx.coroutines.launch
 
 class UserViewModel : ViewModel() {
-    private val _currentUser = MutableLiveData<User?>()
-    val currentUser: LiveData<User?> = _currentUser
+    private val userRepository = UserRepository()
+    
+    private val _currentUser = MutableLiveData<String?>()
+    val currentUser: LiveData<String?> = _currentUser
+
+    // 게시판, 채팅에서 필요한 유저 정보
+    private val _userInfoList = MutableLiveData<List<UserInfo?>>()
+    val userInfoList: LiveData<List<UserInfo?>> = _userInfoList
 
     fun loginUser(loginResponse: LoginResponse.Success) {
-        _currentUser.value = User(
-            uid = loginResponse.uid,
-            userEmail = loginResponse.userEmail,
-            name = loginResponse.name,
-            school = loginResponse.school,
-            major = loginResponse.major,
-            profileImageUrl = loginResponse.profileImageUrl
-        )
+        _currentUser.value = loginResponse.uid
     }
 
     fun logoutUser() {
-        _currentUser.value = null
+        if (isLoggedIn()) _currentUser.value = null
     }
 
-    fun isLoggedIn(): Boolean = currentUser.value != null
+    private fun isLoggedIn(): Boolean = currentUser.value != null
+
+    // 게시판 댓글의 유저 정보
+    fun getUsers(uidList: List<String>?) {
+        if (uidList.isNullOrEmpty()) {
+            _userInfoList.value = emptyList()
+            return
+        }
+
+        viewModelScope.launch {
+            when (val userInfo = userRepository.getUsersInfo(uidList)) {
+                is UserResponse.Success -> _userInfoList.value = userInfo.usersInfo
+                is UserResponse.Error -> _userInfoList.value = emptyList()
+            }
+        }
+    }
 }
